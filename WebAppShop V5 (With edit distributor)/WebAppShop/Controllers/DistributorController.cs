674c95d98aed9cac6f98e29f852380630283dfd8
Oleg <71179786+OlegUrsuliak@users.noreply.github.com>
2023-05-02 21:08:35 +0300

    Basic actions with a sign "Distributor"
    Currently, my application can create new records, display them, delete them, and edit them. (Work ONLY with table "Distributor")

---
+++ WebAppShop V5 (With edit distributor)/WebAppShop/Controllers/DistributorController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppShop.Data;
+using WebAppShop.Models;
+
+namespace WebAppShop.Controllers
+{
+    public class DistributorController : Controller
+    {
+        ApplicationDbContext context;
+        //Конструктор
+        public DistributorController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        //Відображення всієї інформації з таблички
+        public IActionResult Index()
+        {
+            return View(context.Distributors);
+        }
+
+        //Створення одного item
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Models.Distributor distributors)
+        {
+                context.Distributors.Add(distributors);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+        }
+
+        //Видалення одного елементу
+        public async Task <IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Distributor distributors = await context.Distributors.FirstOrDefaultAsync(p => p.Id == id);
+                if (distributors != null)
+                    return View(distributors);
+            }
+            return NotFound();
+        }
+        public async Task<IActionResult> Delete(int ? id)
+        {
+            if (id != null)
+            {
+                Distributor distributors = await context.Distributors.FirstOrDefaultAsync(p => p.Id == id);
+                if (distributors != null)
+                {
+                    context.Distributors.Remove(distributors);
+                    await context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                } 
+            }
+                return NotFound();
+        }
+
+        //Редагування
+        public async Task <IActionResult> Edit (int? id)
+        {
+            if (id!=null)
+            {
+                Distributor distributor = await context.Distributors.FirstOrDefaultAsync(p => p.Id == id);
+                if (distributor != null)
+                    return View(distributor);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Distributor distributor)
+        {
+            context.Distributors.Update(distributor);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ WebAppShop V5 (With edit distributor)/WebAppShop/Models/Distributor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppShop.Models
+{
+    public class Distributor
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Company { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Arrears { get; set; }
+        public string Notes { get; set; }
+    }
+}
